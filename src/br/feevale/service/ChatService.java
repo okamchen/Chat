9874e0414154b2c95467d88473e77b0d26d81e9f
Oelton <oelton.kamchen@gmail.com>
2016-11-06 17:37:41 -0200

    Agora vaiiii

---
+++ src/br/feevale/service/ChatService.java
@@ -0,0 +1,286 @@
+package br.feevale.service;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.LayoutStyle;
+import javax.swing.WindowConstants;
+
+public class ChatService extends JFrame {
+	
+	ArrayList clientOutputStreams;
+	ArrayList<String> users;
+
+	// Variables declaration - do not modify//GEN-BEGIN:variables
+	private JButton btnLimpar;
+	private JButton btnFim;
+	private JButton btnInicio;
+	private JButton btnUsuarios;
+	private JScrollPane jScrollPane1;
+	private JTextArea taChat;
+	// End of variables declaration//GEN-END:variables
+
+	public class ClientHandler implements Runnable {
+		BufferedReader reader;
+		Socket sock;
+		PrintWriter client;
+
+		public ClientHandler(Socket clientSocket, PrintWriter user) {
+			client = user;
+			try {
+				sock = clientSocket;
+				InputStreamReader isReader = new InputStreamReader(sock.getInputStream());
+				reader = new BufferedReader(isReader);
+			} catch (Exception ex) {
+				taChat.append("Unexpected error... \n");
+			}
+
+		}
+
+		@Override
+		public void run() {
+			String message, connect = "Connect", disconnect = "Disconnect", chat = "Chat";
+			String[] data;
+
+			try {
+				while ((message = reader.readLine()) != null) {
+					taChat.append("Recebido: " + message + "\n");
+					data = message.split(":");
+
+					for (String token : data) {
+						taChat.append(token + "\n");
+					}
+
+					if (data[2].equals(connect)) {
+						log((data[0] + ":" + data[1] + ":" + chat));
+						userAdd(data[0]);
+					} else if (data[2].equals(disconnect)) {
+						log((data[0] + ": está desconectado." + ":" + chat));
+						userRemove(data[0]);
+					} else if (data[2].equals(chat)) {
+						log(message);
+					} else {
+						taChat.append("Nenhum condição encontrada. \n");
+					}
+				}
+			} catch (Exception ex) {
+				taChat.append("Perda de conexão. \n");
+				ex.printStackTrace();
+				clientOutputStreams.remove(client);
+			}
+		}
+	}
+
+	public ChatService() {
+		initComponents();
+	}
+
+	private void initComponents() {
+
+		jScrollPane1 = new JScrollPane();
+		taChat = new JTextArea();
+		btnInicio = new JButton();
+		btnFim = new JButton();
+		btnUsuarios = new JButton();
+		btnLimpar = new JButton();
+
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setTitle("Chat - Server's frame");
+		setName("server"); // NOI18N
+		setResizable(false);
+
+		taChat.setColumns(20);
+		taChat.setRows(5);
+		jScrollPane1.setViewportView(taChat);
+
+		btnInicio.setText("START");
+		btnInicio.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				b_startActionPerformed(evt);
+			}
+		});
+
+		btnFim.setText("END");
+		btnFim.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				b_endActionPerformed(evt);
+			}
+		});
+
+		btnUsuarios.setText("Online Users");
+		btnUsuarios.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				b_usersActionPerformed(evt);
+			}
+		});
+
+		btnLimpar.setText("Clear");
+		btnLimpar.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				b_clearActionPerformed(evt);
+			}
+		});
+
+		GroupLayout layout = new GroupLayout(getContentPane());
+		getContentPane().setLayout(layout);
+		layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+				.addGroup(layout
+						.createSequentialGroup().addContainerGap().addGroup(layout
+								.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(
+										jScrollPane1)
+								.addGroup(layout.createSequentialGroup()
+										.addGroup(layout
+												.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+												.addComponent(btnFim, GroupLayout.DEFAULT_SIZE,
+														GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+												.addComponent(btnInicio, GroupLayout.DEFAULT_SIZE, 75,
+														Short.MAX_VALUE))
+										.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 291,
+												Short.MAX_VALUE)
+										.addGroup(layout
+												.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+												.addComponent(btnLimpar, GroupLayout.DEFAULT_SIZE,
+														GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+												.addComponent(btnUsuarios, GroupLayout.DEFAULT_SIZE, 103,
+														Short.MAX_VALUE))))
+						.addContainerGap())
+				.addGroup(GroupLayout.Alignment.TRAILING,
+						layout.createSequentialGroup()
+								.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addGap(209, 209, 209)));
+		layout.setVerticalGroup(
+				layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+						.addGroup(
+								layout.createSequentialGroup().addContainerGap()
+										.addComponent(jScrollPane1, GroupLayout.DEFAULT_SIZE, 340,
+												Short.MAX_VALUE)
+										.addGap(18, 18, 18)
+										.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+												.addComponent(btnInicio).addComponent(btnUsuarios))
+										.addGap(18, 18, 18)
+										.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+												.addComponent(btnLimpar).addComponent(btnFim))
+										.addGap(4, 4, 4)));
+
+		pack();
+	}
+
+	private void b_endActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_b_endActionPerformed
+		try {
+			Thread.sleep(5000); // 5000 milliseconds is five second.
+		} catch (InterruptedException ex) {
+			Thread.currentThread().interrupt();
+		}
+
+		log("Server: está parado e todos os usuários estão desconectados.\n:Chat");
+		taChat.append("Server stopping... \n");
+
+		taChat.setText("");
+	}
+
+	private void b_startActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_b_startActionPerformed
+		Thread starter = new Thread(new ServerStart());
+		starter.start();
+
+		taChat.append("Servidor iniciado...\n");
+	}
+
+	private void b_usersActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_b_usersActionPerformed
+		taChat.append("\n Usuários online : \n");
+		for (String current_user : users) {
+			taChat.append(current_user);
+			taChat.append("\n");
+		}
+
+	}
+
+	private void b_clearActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_b_clearActionPerformed
+		taChat.setText("");
+	}
+
+	public static void main(String args[]) {
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new ChatService().setVisible(true);
+			}
+		});
+	}
+
+	public class ServerStart implements Runnable {
+		@Override
+		public void run() {
+			clientOutputStreams = new ArrayList();
+			users = new ArrayList<String>();
+
+			try {
+				ServerSocket serverSock = new ServerSocket(2222);
+				while (true) {
+					Socket clientSock = serverSock.accept();
+					PrintWriter writer = new PrintWriter(clientSock.getOutputStream());
+					clientOutputStreams.add(writer);
+
+					Thread listener = new Thread(new ClientHandler(clientSock, writer));
+					listener.start();
+					taChat.append("Feita conexão. \n");
+				}
+			} catch (Exception ex) {
+				taChat.append("Error ao conectar. \n");
+			}
+		}
+	}
+
+	public void userAdd(String data) {
+		String message, add = ": :Conectado", done = "Server: :Done", name = data;
+		users.add(name);
+		String[] tempList = new String[(users.size())];
+		users.toArray(tempList);
+
+		for (String token : tempList) {
+			message = (token + add);
+			log(message);
+		}
+		log(done);
+	}
+
+	public void userRemove(String data) {
+		String message, add = ": Conectado", done = "Server: :Done", name = data;
+		users.remove(name);
+		String[] tempList = new String[(users.size())];
+		users.toArray(tempList);
+
+		for (String token : tempList) {
+			message = (token + add);
+			log(message);
+		}
+		log(done);
+	}
+
+	public void log(String message) {
+		Iterator it = clientOutputStreams.iterator();
+
+		while (it.hasNext()) {
+			try {
+				PrintWriter writer = (PrintWriter) it.next();
+				writer.println(message);
+				taChat.append("Enviado: " + message + "\n");
+				writer.flush();
+				taChat.setCaretPosition(taChat.getDocument().getLength());
+
+			} catch (Exception ex) {
+				taChat.append("Error ao escrever o log. \n");
+			}
+		}
+	}
+
+}
---
+++ src/br/feevale/view/ClientChat.java
@@ -0,0 +1,336 @@
+package br.feevale.view;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle;
+import javax.swing.WindowConstants;
+
+public class ClientChat extends JFrame {
+
+	String usuario, url = "localhost";
+	ArrayList<String> usuarios = new ArrayList<String>();
+	int porta = 2222;
+	Boolean isConnected = false;
+
+	Socket sock;
+	BufferedReader reader;
+	PrintWriter writer;
+
+	private JButton btnConextar;
+	private JButton btnDesconctar;
+	private JButton btnEnviar;
+	private JScrollPane jScrollPane1;
+	private JLabel lblUrl;
+	private JLabel lblPorta;
+	private JLabel lblUsuario;
+	private JTextArea taChat;
+	private JTextField txtUrl;
+	private JTextField txtChat;
+	private JTextField txtPorta;
+	private JTextField txtUsuario;
+
+	public static void main(String args[]) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new ClientChat().setVisible(true);
+			}
+		});
+	}
+
+	public void ListenThread() {
+		Thread IncomingReader = new Thread(new IncomingReader());
+		IncomingReader.start();
+	}
+
+	public void addUsuario(String data) {
+		usuarios.add(data);
+	}
+
+	public void removerUsuario(String usuario) {
+		taChat.append(usuario + " está offline.\n");
+	}
+
+	public void writeUsers() {
+		String[] tempList = new String[(usuarios.size())];
+		usuarios.toArray(tempList);
+	}
+
+	public void enviarDesconexao() {
+		String bye = (usuario + ": :Disconnect");
+		try {
+			writer.println(bye);
+			writer.flush();
+		} catch (Exception e) {
+			taChat.append("Erro ao enviar mensagem de desconexão.\n");
+		}
+	}
+
+	public void desconectar() {
+		try {
+			taChat.append("Desconectado.\n");
+			sock.close();
+		} catch (Exception ex) {
+			taChat.append("Falha ao desconectar. \n");
+		}
+		isConnected = false;
+		txtUsuario.setEditable(true);
+
+	}
+
+	public ClientChat() {
+		inicializarTela();
+	}
+
+	public class IncomingReader implements Runnable {
+		@Override
+		public void run() {
+			String[] data;
+			String stream, done = "Done", connect = "Conectado", disconnect = "Desconectado", chat = "Chat";
+
+			try {
+				while ((stream = reader.readLine()) != null) {
+					data = stream.split(":");
+
+					if (data[2].equals(chat)) {
+						taChat.append(data[0] + ": " + data[1] + "\n");
+						taChat.setCaretPosition(taChat.getDocument().getLength());
+					} else if (data[2].equals(connect)) {
+						taChat.removeAll();
+						addUsuario(data[0]);
+					} else if (data[2].equals(disconnect)) {
+						removerUsuario(data[0]);
+					} else if (data[2].equals(done)) {
+						// users.setText("");
+						writeUsers();
+						usuarios.clear();
+					}
+				}
+			} catch (Exception ex) {
+			}
+		}
+	}
+
+	private void inicializarTela() {
+
+		lblUrl = new JLabel();
+		txtUrl = new JTextField();
+		lblPorta = new JLabel();
+		txtPorta = new JTextField();
+		lblUsuario = new JLabel();
+		txtUsuario = new JTextField();
+		btnConextar = new JButton();
+		btnDesconctar = new JButton();
+		jScrollPane1 = new JScrollPane();
+		taChat = new JTextArea();
+		txtChat = new JTextField();
+		btnEnviar = new JButton();
+
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setTitle("Chat Java");
+		setResizable(false);
+		
+		lblUrl.setText("URL : ");
+		txtUrl.setText("localhost");
+		txtUrl.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtUrlActionPerformed(evt);
+            }
+        });
+
+		lblPorta.setText("Porta :");
+		txtPorta.setText("2222");
+		txtPorta.addActionListener(new ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtPortaActionPerformed(evt);
+            }
+        });
+
+		lblUsuario.setText("Usuário :");
+		txtUsuario.addActionListener(new ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtUsuarioActionPerformed(evt);
+            }
+        });
+
+		btnConextar.setText("Conectar");
+		btnConextar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				conectar(evt);
+			}
+		});
+
+		btnDesconctar.setText("Desconectar");
+		btnDesconctar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				disconectar(evt);
+			}
+		});
+
+		taChat.setColumns(20);
+		taChat.setRows(5);
+		jScrollPane1.setViewportView(taChat);
+
+		btnEnviar.setText("SEND");
+		btnEnviar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				enviarMsg(evt);
+			}
+		});
+
+
+		GroupLayout layout = new GroupLayout(getContentPane());
+		getContentPane().setLayout(layout);
+
+		layout.setHorizontalGroup(layout.createParallelGroup(Alignment.LEADING)
+				.addGroup(
+						layout.createSequentialGroup().addContainerGap()
+								.addGroup(layout
+										.createParallelGroup(Alignment.LEADING).addGroup(layout
+												.createSequentialGroup().addComponent(txtChat,
+														GroupLayout.PREFERRED_SIZE,
+														352, GroupLayout.PREFERRED_SIZE)
+												.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+												.addComponent(btnEnviar, GroupLayout.DEFAULT_SIZE, 111,
+														Short.MAX_VALUE))
+										.addComponent(jScrollPane1)
+										.addGroup(layout.createSequentialGroup().addGroup(layout
+												.createParallelGroup(Alignment.TRAILING, false)
+												.addComponent(lblUsuario, GroupLayout.DEFAULT_SIZE, 62,
+														Short.MAX_VALUE)
+												.addComponent(lblUrl, GroupLayout.DEFAULT_SIZE,
+														GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+												.addGap(18, 18, 18)
+												.addGroup(layout
+														.createParallelGroup(Alignment.LEADING,
+																false)
+														.addComponent(txtUrl, GroupLayout.DEFAULT_SIZE, 89,
+																Short.MAX_VALUE)
+														.addComponent(txtUsuario))
+												.addGap(18, 18, 18)
+												.addGroup(layout
+														.createParallelGroup(Alignment.LEADING,
+																false)
+														.addComponent(lblPorta, GroupLayout.DEFAULT_SIZE,
+																GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+												.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+												.addGroup(layout.createParallelGroup(
+														Alignment.LEADING, false).addComponent(
+																txtPorta, GroupLayout.DEFAULT_SIZE, 50,
+																Short.MAX_VALUE))
+												.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+												.addGroup(layout
+														.createParallelGroup(Alignment.LEADING)
+														.addGroup(
+																layout.createSequentialGroup().addComponent(btnConextar)
+																		.addGap(2, 2, 2).addComponent(btnDesconctar)
+																		.addGap(0, 0, Short.MAX_VALUE)))))
+								.addContainerGap())
+				.addGroup(Alignment.TRAILING,
+						layout.createSequentialGroup()
+								.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addGap(201, 201, 201)));
+
+		layout.setVerticalGroup(layout.createParallelGroup(Alignment.LEADING).addGroup(layout
+				.createSequentialGroup().addContainerGap()
+				.addGroup(layout.createParallelGroup(Alignment.BASELINE).addComponent(lblUrl)
+						.addComponent(txtUrl, GroupLayout.PREFERRED_SIZE,
+								GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblPorta).addComponent(txtPorta, GroupLayout.PREFERRED_SIZE,
+								GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+				.addGroup(layout.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(txtUsuario)
+						.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(lblUsuario).addComponent(btnConextar)
+								.addComponent(btnDesconctar)))
+				.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+				.addComponent(jScrollPane1, GroupLayout.PREFERRED_SIZE, 310,
+						GroupLayout.PREFERRED_SIZE)
+				.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+				.addGroup(layout.createParallelGroup(Alignment.LEADING).addComponent(txtChat)
+						.addComponent(btnEnviar, GroupLayout.DEFAULT_SIZE, 31, Short.MAX_VALUE))
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)));
+		pack();
+	}
+
+	private void conectar(ActionEvent evt) {
+		if (isConnected == false) {
+			usuario = txtUsuario.getText();
+			txtUsuario.setEditable(false);
+
+			try {
+				sock = new Socket(url, porta);
+				InputStreamReader streamreader = new InputStreamReader(sock.getInputStream());
+				reader = new BufferedReader(streamreader);
+				writer = new PrintWriter(sock.getOutputStream());
+				writer.println(usuario + ": está conectado.:Connect");
+				writer.flush();
+				isConnected = true;
+			} catch (Exception ex) {
+				taChat.append("Erro de conexão! Tente novamente. \n");
+				txtUsuario.setEditable(true);
+			}
+
+			ListenThread();
+
+		} else if (isConnected == true) {
+			taChat.append("Você já está conectado. \n");
+		}
+	}
+
+	private void disconectar(ActionEvent evt) {
+		enviarDesconexao();
+		desconectar();
+	}
+
+	private void enviarMsg(ActionEvent evt) {
+		String nothing = "";
+		if ((txtChat.getText()).equals(nothing)) {
+			txtChat.setText("");
+			txtChat.requestFocus();
+		} else {
+			try {
+				writer.println(usuario + ":" + txtChat.getText() + ":" + "Chat");
+				writer.flush();
+			} catch (Exception ex) {
+				taChat.append("Erro ao enviar mensagem. \n");
+			}
+			txtChat.setText("");
+			txtChat.requestFocus();
+		}
+
+		txtChat.setText("");
+		txtChat.requestFocus();
+	}
+	
+	private void txtUrlActionPerformed(java.awt.event.ActionEvent evt) {
+	   
+	}
+
+    private void txtPortaActionPerformed(ActionEvent evt) {
+    	   
+    }
+
+    private void txtUsuarioActionPerformed(ActionEvent evt) {
+    
+    }
+
+
+
+}
